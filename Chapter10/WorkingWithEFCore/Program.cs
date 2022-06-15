d1b63ce5a22610a0bd0521a0ff6e059f206fa37c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-15 20:33:40 +0300

    Wrote some basic code

--- Chapter10/WorkingWithEFCore/Program.cs
+++ Chapter10/WorkingWithEFCore/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Packt.Shared;
+
+using static System.Console;
+
+namespace Code2.Chapter10.WorkingWithEFCore;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WriteLine($"Using {ProjectConstants.DatabaseProvider} database provider.");
+    }
+}
