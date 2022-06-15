0d8bcd4707c2320b90c766e2f634c4c1c1331990
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-15 17:16:18 +0300

    Added the logic

--- Chapter03/HandlingExceptions/Program.cs
+++ Chapter03/HandlingExceptions/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq.Expressions;
+using static System.Console;
+
+namespace Code2.Chapter03.HandlingExceptions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WriteLine("Before parsing");
+        Write("What is your age?");
+        string? input = ReadLine();
+
+        try
+        {
+            int age = int.Parse(input);
+            WriteLine($"You are {age} years old");
+        }
+        catch(OverflowException)
+        {
+            WriteLine("The age you entered is in the valid format but the value is too big");
+        }
+        catch (FormatException)
+        {
+            WriteLine("The age you entered is not in the valid format");
+        }
+        catch (System.Exception ex)
+        {
+            WriteLine($"{ex.GetType()} says {ex.Message}");
+        }
+    }
+}
