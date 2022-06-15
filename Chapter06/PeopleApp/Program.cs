e08f2944c6bf51876a5b7997fd64c5e052edf158
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-15 18:18:17 +0300

    Added logic to the Program file

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PacktLibrary;
+
+using static System.Console;
+
+namespace Code2.Chapter06.PeopleApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Person harry = new()
+        {
+            Name = "Harry"
+        };
+
+        Person mary = new()
+        {
+            Name = "Mary"
+        };
+
+        Person jill = new()
+        {
+            Name = "Jill"
+        };
+
+        //Call instance method
+        Person baby1 = mary.ProcreateWith(harry);
+        baby1.Name = "Gary";
+
+        //Call static method
+        Person baby2 = Person.Procreate(harry, jill);
+    }
+}
