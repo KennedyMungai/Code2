5bdb38b6e73cfb9967b41273472ab3b620326bbe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-15 15:22:44 +0300

    Added the Operators console application

---
+++ Chapter03/Operators/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+namespace Code2.Chapter03.Operators;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int a = 3;
+        int b = a++;
+
+        WriteLine($"a is {a} and b is {b}");
+    }
+}
