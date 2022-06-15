9b1b3415dce9580a3ac662c70ecdb66f2d3f00d3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-15 21:20:17 +0300

    Added logic to the file

--- Chapter11/LinqWithObjects/Program.cs
+++ Chapter11/LinqWithObjects/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+namespace Code2.Chapter11.LinqWithObjects;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] names = new[]
+        {
+            "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed"
+        };
+
+        WriteLine("Deferred Execution");
+
+        var query1 = names.Where(name => name.EndsWith("m"));
+
+        foreach (var name in query1)
+        {
+            WriteLine(name);
+        }
+    }
+}
