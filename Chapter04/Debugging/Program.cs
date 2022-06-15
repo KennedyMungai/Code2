5a2e4913f86095e8aad5433993b1b89a3ff7554a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-15 15:30:49 +0300

    Added the debugging console application

---
+++ Chapter04/Debugging/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+namespace Code2.Chapter04.Debugging;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        double a = 4.5;
+        double b = 2.5;
+        double answer = Add(a, b);
+        WriteLine($"{a} + {b} = {answer}");
+    }
+
+    static double Add(double a, double b)
+    {
+        return a* b;
+    }
+}
