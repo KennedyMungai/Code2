42469a05987cea8f73b07087beaffaf2356ed573
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-15 21:11:47 +0300

    Added a QueryingCategories method

--- Chapter10/WorkingWithEFCore/Program.cs
+++ Chapter10/WorkingWithEFCore/Program.cs
@@ -1,4 +1,5 @@
-﻿using Packt.Shared;
+﻿using Microsoft.EntityFrameworkCore;
+using Packt.Shared;
 
 using static System.Console;
 
@@ -8,6 +9,32 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        WriteLine($"Using {ProjectConstants.DatabaseProvider} database provider.");
+        // WriteLine($"Using {ProjectConstants.DatabaseProvider} database provider.");
+
+        QueryingCategories();
+    }
+
+    static void QueryingCategories()
+    {
+        using (Northwind db = new())
+        {
+            WriteLine("Categories and howmany products they have");
+
+            //A query to get all categories and their related products
+            IQueryable<Category>? categories = db.Categories?
+                .Include(c => c.Products);
+
+            if (categories is null)
+            {
+                WriteLine("No Categories found.");
+                return;
+            }
+
+            //Execute Query and Enumerate the results
+            foreach (Category c in categories)
+            {
+                WriteLine($"{c.CategoryName} has {c.Products.Count} products");
+            }
+        }
     }
 }
