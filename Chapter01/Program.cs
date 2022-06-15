9273f0ecc8046ee799262dfd7f7f152c5f961beb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-15 14:37:49 +0300

    Added Program body

--- Chapter01/Program.cs
+++ Chapter01/Program.cs
@@ -1,2 +1,11 @@
-﻿namespace Chapter01;
+﻿using static System.Console;
 
+namespace Chapter01;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WriteLine("Another Hello World");
+    }
+}
