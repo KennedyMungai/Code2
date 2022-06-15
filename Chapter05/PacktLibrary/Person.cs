249225b5dfba5d47a8197a5889cd50ce658e8f57
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-15 18:33:56 +0300

    Added the Harry_Shout method

--- Chapter05/PacktLibrary/Person.cs
+++ Chapter05/PacktLibrary/Person.cs
@@ -9,4 +9,15 @@ public class Person
     public WondersOfTheAncientWorld favouriteAncientWonder;
 
     public List<Person> Children = new List<Person>();
+
+    static void Harry_Shout(object? sender, EventArgs e)
+    {
+        if (sender is null)
+        {
+            return;
+        }
+
+        Person p =(Person)sender;
+        WriteLine($"{p.Name} is this angry: {p.AngerLevel}");
+    }   
 }
