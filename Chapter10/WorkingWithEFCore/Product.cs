ef13c05a98b1a2b66ec15478e4ac6839949768fe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-15 20:43:16 +0300

    Added some fields to the model

--- Chapter10/WorkingWithEFCore/Product.cs
+++ Chapter10/WorkingWithEFCore/Product.cs
@@ -1,6 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Packt.Shared;
 
 public class Product
 {
+    public int ProductId { get; set; }
     
+    [Required]
+    [StringLength(40)]
+    public string ProductName { get; set; } = null!;
+    
+    [Column(Name="UnitPrice", TypeName="money")]
+    public decimal? Cost { get; set; }
+    
+    [Column(Name = "UnitsInStock")]
+    public short? Stock { get; set; }
+    
+    public bool Discontinued { get; set; }
+    
+    
+    //These two define a foreign key relationship to the categories table
+    public int CategoryId { get; set; }
+    public virtual Category Category { get; set; } = null!;
 }
