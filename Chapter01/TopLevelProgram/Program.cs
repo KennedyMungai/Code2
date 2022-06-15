c25247bf7862d42a2cf633474d14e836d9f93602
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-15 14:44:39 +0300

    Added some logic

--- Chapter01/TopLevelProgram/Program.cs
+++ Chapter01/TopLevelProgram/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+namespace Code2.Chapter01.TopLevelProgram;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WriteLine("Hello from a top level program");
+        WriteLine(Environment.OSVersion.VersionString);
+    }
+}
