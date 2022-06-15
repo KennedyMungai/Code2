8a78bb8bade74eae68ac1978024a075d0a910700
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-15 20:48:30 +0300

    Added DbSet fields and OnModelCreating

--- Chapter10/WorkingWithEFCore/Northwind.cs
+++ Chapter10/WorkingWithEFCore/Northwind.cs
@@ -1,3 +1,4 @@
+using System.Reflection.Emit;
 using Microsoft.EntityFraneworkCore;
 
 using static System.Console;
@@ -7,6 +8,9 @@ namespace Packt.Shared;
 //This manages the connection to the database
 public class Northwind : DbContext
 {
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (ProjectConstants.DatabaseProvider == "SqlServer")
@@ -14,5 +18,14 @@ public class Northwind : DbContext
             string connection = "Data Source = KENNEDYMUNGAI; Initial Catalog=Northwind; Trusted_Connection=true; MultipleActiveResultSets=true;";
             optionsBuilder.UseSqlServer(connection);
         }
+    }
+
+    protected override void OnModelCreating(ModuleBuilder modelBuilder)
+    {
+        //An example of using Fluent API instead of attributes to limit the length of a category name to 15
+        modelBuilder.Entity<Category>()
+            .Property(category => category.CategoryName)
+            .IsRequired()               // Is not null
+            .HasMaxLength(15);
     }
 }
