a500126aacf7cbb1a78ee3194638f7698d95881b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-15 18:32:52 +0300

    Added Harry_Shout method to be called by events

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -30,4 +30,15 @@ public class Program
         //Call static method
         Person baby2 = Person.Procreate(harry, jill);
     }
+
+    static void Harry_Shout(object? sender, EventArgs e)
+    {
+        if (sender is null)
+        {
+            return;
+        }
+
+        Person p =(Person)sender;
+        WriteLine($"{p.Name} is this angry: {p.AngerLevel}");
+    }
 }
