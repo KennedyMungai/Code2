cbc2eea1993b612a8af46799077ebb6bd249d254
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-15 18:10:18 +0300

    Added the static Procreate method

--- Chapter06/PacktLibrary/Person.cs
+++ Chapter06/PacktLibrary/Person.cs
@@ -1,6 +1,8 @@
-﻿using static System.Console;
+﻿using System.Security.Cryptography;
+using static System.Console;
 
 namespace PacktLibrary;
+
 
 public class Person : Object
 {
@@ -13,4 +15,20 @@ public class Person : Object
     {
         WriteLine($"{Name} was born on {DateOfBirth.ToLongDateString()}");
     }
+
+    
+    public static Person Procreate(Person p1, Person p2)
+    {
+        Person baby = new
+        {
+            Name=$"Baby of {p1.Name} and {p2.Name}"
+        }
+
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+
+        return baby;
+    }
+
+
 }
