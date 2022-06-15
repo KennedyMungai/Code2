07de21079b2c0b5e0da925c8d5e22be6056a379f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-15 18:05:54 +0300

    Added some basic code

--- Chapter06/PacktLibrary/Person.cs
+++ Chapter06/PacktLibrary/Person.cs
@@ -1,6 +1,16 @@
-﻿namespace PacktLibrary;
+﻿using static System.Console;
 
-public class Person
+namespace PacktLibrary;
+
+public class Person : Object
 {
+    //fields
+    public string? Name;
+    public DateTime DateOfBirth;
+    public List<Person> Children = new();
 
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on {DateOfBirth.ToLongDateString()}");
+    }
 }
