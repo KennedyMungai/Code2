90d52a0ec16cdafec476a38f48763ed2d9a691b0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-15 20:42:59 +0300

    Added some fields to the model

--- Chapter10/WorkingWithEFCore/Category.cs
+++ Chapter10/WorkingWithEFCore/Category.cs
@@ -1,6 +1,23 @@
+using System.ComponentModel.DataAnnotations.Schema;             //[Column]
+
 namespace Packt.Shared;
 
 public class Category
 {
+    //These properties map to columns in the database
+    public int CategoryId { get; set; }
+    public string? CategoryName { get; set; }
     
+    [Column(TypeName = "ntext")]
+    public string? Description { get; set; }
+    
+    //Defines a navigation property for related rows
+    public virtual ICollection<Product> Products { get; set; }
+    
+    public Category()
+    {
+        //To enable developers to add products to a Category, we must 
+        //initialize the navigation property to an empty collection
+        Products = new HashSet<Products>();
+    }
 }
