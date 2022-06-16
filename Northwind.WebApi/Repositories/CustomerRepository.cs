768d423aa6318c403f16f917377c5b9e24eae993
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-16 15:08:51 +0300

    Implemented the interface

--- Northwind.WebApi/Repositories/CustomerRepository.cs
+++ Northwind.WebApi/Repositories/CustomerRepository.cs
@@ -6,5 +6,28 @@ namespace Northwind.WebApi.Repositories;
 
 public class CustomerRepository : ICustomerRepository
 {
-    
+    public Task<Customer?> CreateAsync(Customer c)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Customer>> RetrieveAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Customer?> RetrieveAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Customer?> UpdateAsync(string id, Customer c)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool?> DeleteAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
 }
