3212ba64819865be32f07ddbd1753c9f88c5af34
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-16 13:05:30 +0300

    Added a few services

--- Northwind.WebApi/Program.cs
+++ Northwind.WebApi/Program.cs
@@ -1,3 +1,9 @@
+using System.Security.AccessControl;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Packt.Shared;
+
+using static System.Console;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,33 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddNorthwindContext();
+builder.Services.AddControllers(
+    options =>
+    {
+        WriteLine("Default output formatters:");
+
+        foreach (IOutputFormatter formatter in options.OutputFormatters)
+        {
+            OutputFormatter? mediaFormatter = formatter as OutputFormatter;
+
+            if (mediaFormatter == null)
+            {
+                WriteLine($"    {formatter.GetType().Name}");
+            }
+            else    //OutputFormatter class has SupportedMediaTypes
+            {
+                WriteLine(
+                    "{0}, Media types: {1}",
+                    arg0: mediaFormatter.GetType().Name,
+                    arg1: string.Join(", ", mediaFormatter.SupportedMediaTypes)
+                );
+            }
+        }
+    }
+)
+.AddXmlDataContractSerializerFormatters()
+.AddXmlSerializerFormatters();
 
 var app = builder.Build();
 
