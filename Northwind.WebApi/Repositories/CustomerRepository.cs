97452438543877067a276434217869f5a5583bdd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-16 13:56:17 +0300

    ICustomerService implementation

---
+++ Northwind.WebApi/Repositories/CustomerRepository.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore.ChangeTracking;         // EntityEntry<T>
+using Packt.Shared;                                         // Customer
+using System.Collections.Concurrent;                        //ConcurrentDictionary
+
+namespace Northwind.WebApi.Repositories;
+
+public class CustomerRepository : ICustomerRepository
+{
+    
+}
